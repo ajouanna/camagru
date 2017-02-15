c611f23a79e02f32fda555441f460781130f4644
Antoine JOUANNAIS <ajouanna@student.42.fr>
2017-02-15 18:11:17 +0100

    travail en cours sur generate_image.php

--- control/generate_image.php
+++ control/generate_image.php
@@ -13,16 +13,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	else
 	{
 		$timestamp = mktime();
-		$dir = '../data';
+		$dir = '../data/';
 		$filename = $dir.$timestamp.'.png';
 		$parts = explode(',', $image);
 		$data = $parts[1];
 		$data = base64_decode($data);
 
 		file_put_contents($filename, $data);
-		echo "DEBUG : A FAIRE ! mixer les images avec imagecopy";
-
-		return;
+		$dessous = imagecreatefrompng($filename); //on ouvre l'image source
+		$dessus = imagecreatefrompng($image_incrustee); //on ouvre l'image source
+		$infosize = getimagesize($filename); // on recupere la taille dans un array
+		$width_dessous = $infosize[0];
+		$height_dessous = $infosize[1];
+		$dst_x = 0;
+		$dst_y = 0;
+		$src_x = 0;
+		$src_y = 0;
+		$src_w = $width_dessous;
+		$src_h = $height_dessous;
+		$result = imagecopy ( $dessous, $dessus, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h );
+		imagepng($dessous, $filename); // on ecrit l'image traitee $dest vers le fichier $file
 	}
 }
 echo 'Erreur de PHP quelque part...';
