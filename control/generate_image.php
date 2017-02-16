4116d6e57429ded7ff229833bdddb936993f0f0b
Antoine JOUANNAIS <ajouanna@student.42.fr>
2017-02-16 12:50:45 +0100

    amelioration fusion d images

--- control/generate_image.php
+++ control/generate_image.php
@@ -17,22 +17,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		$filename = $dir.$timestamp.'.png';
 		$parts = explode(',', $image);
 		$data = $parts[1];
+		// remplacer les blancs par des + quand les donnees viennent de Javascript canvas.toDataURL() 
+		$data = str_replace(' ','+',$data); 
 		$data = base64_decode($data);
 
-		file_put_contents($filename, $data);
-		$dessous = imagecreatefrompng($filename); //on ouvre l'image source
-		$dessus = imagecreatefrompng($image_incrustee); //on ouvre l'image source
-		$infosize = getimagesize($filename); // on recupere la taille dans un array
-		$width_dessous = $infosize[0];
-		$height_dessous = $infosize[1];
+		file_put_contents($filename, $data); // ecriture de l'image de fond
+		$dessous = imagecreatefrompng($filename); //on ouvre l'image de fond
+		$dessus = imagecreatefrompng($image_incrustee); //on ouvre l'image a incruster
+		$infosize_dessous = getimagesize($filename); // on recupere la taille de l'image de fond dans un array
+		$infosize_dessus = getimagesize($image_incrustee); 
+		$width_dessous = $infosize_dessous[0];
+		$height_dessous = $infosize_dessous[1];
+		$width_dessus = $infosize_dessus[0];
+		$height_dessus = $infosize_dessus[1];
+
 		$dst_x = 0;
 		$dst_y = 0;
 		$src_x = 0;
 		$src_y = 0;
-		$src_w = $width_dessous;
-		$src_h = $height_dessous;
-		$result = imagecopy ( $dessous, $dessus, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h );
+		$src_w = $width_dessus;
+		$src_h = $height_dessus;
+		$dst_w = $width_dessous;
+		$dst_h = $height_dessous;
+		$result = imagecopyresampled ($dessous, $dessus, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h );
 		imagepng($dessous, $filename); // on ecrit l'image traitee $dest vers le fichier $file
+		return;
 	}
 }
 echo 'Erreur de PHP quelque part...';
