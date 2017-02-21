09d037a4696a0122473fd7bd8a71275b3f3bdf9d
xmillero <xavier@milleron.com>
2017-02-21 18:30:54 +0100

    Modification Titre Adm Site

--- view/content_admin.php
+++ view/content_admin.php
@@ -2,7 +2,7 @@
 <div class="content_index">
 <h2>Page d'administration du site</h2>
 	<div class = 'user_list'>
-	<p>Liste de tous les utilisateurs</p>
+	<p>Liste de tous les utilisateurs (cliquez sur un login pour le supprimer)</p>
 	<?PHP
 	list_users();
 	?>
