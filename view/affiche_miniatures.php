3e28bae5a24b3914339c060e72012b03c7f42e5a
xmillero <xavier@milleron.com>
2017-02-23 12:14:24 +0100

    css

--- view/affiche_miniatures.php
+++ view/affiche_miniatures.php
@@ -3,7 +3,7 @@ $dir = '../superposables/*.{jpg,jpeg,gif,png}';
 
 $files = glob($dir,GLOB_BRACE);
   
-echo 'Listing des images du repertoire superposables <br />';
+echo '<br /> <h3> Listing des images du repertoire superposables <h3> <br /> <br />';
 foreach($files as $image)
 { 
 	echo "<li><img class='vignette' src='$image' onclick='select_image(this)'></li>";
